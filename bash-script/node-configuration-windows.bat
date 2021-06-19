b2d7d647926951a432fab95e82542b7fe6b2883a
spatrame <shashidhara.p@tetherfi.com>
2021-06-19 18:06:10 +0530

    changed IP address to localhost ip

--- bash-script/node-configuration-windows.bat
+++ bash-script/node-configuration-windows.bat
@@ -2,34 +2,34 @@
 
 :: Master Ip & Port configuration
 set masterLen=3 
-set master[0].Ip=172.16.3.28
+set master[0].Ip=127.0.0.1
 set master[0].Port=6379
 set master[0].SlotFrom=0
 set master[0].SlotTo=5400
 
-set master[1].Ip=172.16.3.169
+set master[1].Ip=127.0.0.1
 set master[1].Port=6380
 set master[1].SlotFrom=5401
 set master[1].SlotTo=10800
 
-set master[2].Ip=172.16.3.169
+set master[2].Ip=127.0.0.1
 set master[2].Port=6381
 set master[2].SlotFrom=10801
 set master[2].SlotTo=16383
 
 :: Slave IP and Port Configuration
 set slaveLen=3
-set slave[0].Ip=172.16.3.169
+set slave[0].Ip=127.0.0.1
 set slave[0].Port=7379
-set slave[0].MasterIp=172.16.3.28
+set slave[0].MasterIp=127.0.0.1
 set slave[0].MasterPort=6379
 
-set slave[1].Ip=172.16.3.169
+set slave[1].Ip=127.0.0.1
 set slave[1].Port=7380
-set slave[1].MasterIp=172.16.3.169
+set slave[1].MasterIp=127.0.0.1
 set slave[1].MasterPort=6380
 
-set slave[2].Ip=172.16.3.169
+set slave[2].Ip=127.0.0.1
 set slave[2].Port=7381
-set slave[2].MasterIp=172.16.3.169
+set slave[2].MasterIp=127.0.0.1
 set slave[2].MasterPort=6381
--- shell-script/node-configuration-linux.sh
+++ shell-script/node-configuration-linux.sh
@@ -2,34 +2,34 @@
 
 # Master Ip & Port configuration
 masterLen=3 
-masterIp[0]=172.16.2.52
+masterIp[0]=127.0.0.1
 masterPort[0]=6379
 masterSlotFrom[0]=0
 masterSlotTo[0]=5400
 
-masterIp[1]=172.16.2.52
+masterIp[1]=127.0.0.1
 masterPort[1]=6380
 masterSlotFrom[1]=5401
 masterSlotTo[1]=10800
 
-masterIp[2]=172.16.2.52
+masterIp[2]=127.0.0.1
 masterPort[2]=6381
 masterSlotFrom[2]=10801
 masterSlotTo[2]=16383
 
 # Slave IP and Port Configuration
 slaveLen=3
-slaveIp[0]=172.16.2.52
+slaveIp[0]=127.0.0.1
 slavePort[0]=7379
-slaveMasterIp[0]=172.16.2.52
+slaveMasterIp[0]=127.0.0.1
 slaveMasterPort[0]=6379
 
-slaveIp[1]=172.16.2.52
+slaveIp[1]=127.0.0.1
 slavePort[1]=7380
-slaveMasterIp[1]=172.16.2.52
+slaveMasterIp[1]=127.0.0.1
 slaveMasterPort[1]=6380
 
-slaveIp[2]=172.16.2.52
+slaveIp[2]=127.0.0.1
 slavePort[2]=7381
-slaveMasterIp[2]=172.16.2.52
+slaveMasterIp[2]=127.0.0.1
 slaveMasterPort[2]=6381
